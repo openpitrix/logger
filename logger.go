99b0da63a30d7a1e7456a104ec6d3cc600db29ec
chai2010 <chaishushan@gmail.com>
2018-10-18 17:48:32 +0800

    add pkg doc

--- logger.go
+++ logger.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a Apache license
 // that can be found in the LICENSE file.
 
+// Package logger provides standard logger for OpenPitrix.
 package logger
 
 import (
